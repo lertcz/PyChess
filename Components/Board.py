a1909a0638a1e97e30cb3f143e89089b1037f91a
Michal Šesták <lertcze@gmail.com>
2022-03-09 00:39:51 +0100

    pieces are able to make legal moves

--- Components/Board.py
+++ Components/Board.py
@@ -1,6 +1,24 @@
 import pygame
 
+pygame.mixer.init()
+pygame.mixer.music.load("Assets/Sounds/move.mp3")
+pygame.mixer.music.set_volume(0.7)
+
+#setup of a highlight square
+highlightSquare = pygame.Surface((60,60))
+highlightSquare.set_alpha(128) # alpha level
+highlightSquare.fill((246, 246, 105))  
+
+#setup of a possible move square
+possibleMoveSquare = pygame.Surface((60,60))
+possibleMoveSquare.set_alpha(128) # alpha level
+possibleMoveSquare.fill((255, 0, 0))  
+
 class Board:
+    color = {
+        "b": -1,
+        "w": 1
+    }
     def __init__(self, fen, display, pieces) -> None:
         self.BOARD = self.__loadFEN(fen)
         #[print(x) for x in self.BOARD]
@@ -9,12 +27,13 @@ class Board:
 
         self.LIGHT= (235, 195, 160)
         self.DARK = (130, 95, 64)
-        self.HIGHLIGHT = (246, 246, 105)
+        #self.HIGHLIGHT = (246, 246, 105)
 
         self.holding = None
 
         self.pickupZone = None
         self.dropZone = None
+        self.legal_moves = []
 
     def __loadFEN(self, fen):
         board = []
@@ -43,11 +62,154 @@ class Board:
     def highlighted(self):
         if self.pickupZone:
             file, rank = self.pickupZone
-            pygame.draw.rect(self.display, self.HIGHLIGHT, (rank * 60, file * 60, 60, 60))
+            self.display.blit(highlightSquare, (rank * 60, file * 60))
         
         if self.dropZone:
             file, rank = self.dropZone
-            pygame.draw.rect(self.display, self.HIGHLIGHT, (rank * 60, file * 60, 60, 60))
+            self.display.blit(highlightSquare, (rank * 60, file * 60))
+
+        if self.holding:
+            self.legalMoves()
+            for square in self.legal_moves:
+                file, rank = square
+                self.display.blit(possibleMoveSquare, (rank * 60, file * 60))
+
+    def legalMoves(self):
+        if not self.legal_moves:
+            y, x = self.pickupZone
+            if self.holding.lower() == "p":
+                self.pawnMove(x, y)
+
+            elif self.holding.lower() == "b":
+                self.bishopMove(x, y)
+            
+            elif self.holding.lower() == "n":
+                self.knightMove(x, y)
+
+            elif self.holding.lower() == "r":
+                self.rookMove(x, y)
+
+            elif self.holding.lower() == "k":
+                self.kingMove(x, y)
+
+            elif self.holding.lower() == "q":
+                self.queeenMove(x, y)
+
+    def pawnMove(self, x , y):
+        color = "b" if self.holding.isupper() else "w"
+        orientation = self.color[color]
+        
+        #!TODO promotion
+
+        #!TODO start move by 2
+
+        # base move
+        if 0 <= y + orientation <= 7: # boundary check
+            if not self.BOARD[y + orientation][x]: # only move if nothing is in front
+                self.legal_moves.append([y + orientation, x])
+
+        # elimination
+        # condition to take care of overflow
+        for side in [-1, 1]:
+            if 0 <= x + side <= 7 and 0 <= y + orientation <= 7: # boundary check
+                # if there is a piece on the side and it is an enemy
+                if self.BOARD[y + orientation][x + side] and self.__checkEnemyPiece(color, x + side, y + orientation):
+                    self.legal_moves.append([y + orientation, x + side])
+
+    def __checkEnemyPiece(self, side, x, y):
+        if side == "b":
+            return self.BOARD[y][x].islower()
+        return self.BOARD[y][x].isupper()
+
+    def straightLine(self, side, x, y):
+        for X in [-1, 1]:
+            temp = x
+            while 0 <= temp <= 7:
+                if temp != x: #not self
+                    if self.BOARD[y][temp]: # stop after finding a piece
+                        if self.__checkEnemyPiece(side, temp, y): # add enemy piece to be eliminated
+                            self.legal_moves.append([y, temp])
+                        break
+
+                    self.legal_moves.append([y, temp])
+                temp += X
+            
+        for Y in [-1, 1]:
+            temp = y
+            while 0 <= temp <= 7:
+                if temp != y: #not self
+                    if self.BOARD[temp][x]: # stop after finding a piece
+                        if self.__checkEnemyPiece(side, x, temp): # add enemy piece to be eliminated
+                            self.legal_moves.append([temp, x])
+                        break
+
+                    self.legal_moves.append([temp, x])
+                temp += Y
+
+    def __checkEnemyPieceDiagonal(self, side, MAIN, temp, x, y):
+        if side == "b":
+            return self.BOARD[y + temp][x + (temp * MAIN)].islower()
+        return self.BOARD[y + temp][x + (temp * MAIN)].isupper()
+
+    def diagonalLine(self, side, x, y):
+        for DIAGONAL in [-1, 1]:
+            for MAIN in [-1, 1]:
+                temp = 0
+                while 0 <= x + (temp * MAIN) <= 7 and 0 <= y + temp <= 7:
+                    if temp != 0: #not self
+                        if self.BOARD[y + temp][x + (temp * MAIN)]: # stop after finding a piece
+                            if self.__checkEnemyPieceDiagonal(side, MAIN, temp, x, y): # add enemy piece to be eliminated
+                                self.legal_moves.append([y + temp, x + (temp * MAIN)])
+                            break
+
+                        self.legal_moves.append([y + temp, x + (temp * MAIN)])
+                    temp += DIAGONAL
+
+    def bishopMove(self, x ,y):
+        color = "b" if self.holding.isupper() else "w"
+
+        self.diagonalLine(color, x, y)
+    
+    def knightMove(self, x ,y):
+        color = "b" if self.holding.isupper() else "w"
+
+        for X in [1, -1, 2, -2]:
+            for Y in [1, -1, 2, -2]:
+                if X != Y and X != -Y:
+                    if 0 <= x+X <= 7 and 0 <= y+Y <= 7:
+                        #check for enemy
+                        if self.BOARD[y+Y][x+X]: 
+                            if self.__checkEnemyPiece(color, x + X, y + Y):
+                                self.legal_moves.append([y + Y, x + X])
+                        #check for free space
+                        else:
+                            self.legal_moves.append([y + Y, x + X])
+    
+    def rookMove(self, x ,y):
+        color = "b" if self.holding.isupper() else "w"
+
+        self.straightLine(color, x, y)
+    
+    def kingMove(self, x ,y):
+        color = "b" if self.holding.isupper() else "w"
+
+        for Y in range(-1, 2):
+            for X in range(-1, 2):
+                if X or Y != 0: # not it self
+                    if 0 <= x+X <= 7 and 0 <= y+Y <= 7:
+                        #check for enemy
+                        if self.BOARD[y+Y][x+X]: 
+                            if self.__checkEnemyPiece(color, x + X, y + Y):
+                                self.legal_moves.append([y + Y, x + X])
+                        #check for free space
+                        else:
+                            self.legal_moves.append([y + Y, x + X])
+    
+    def queeenMove(self, x ,y):
+        color = "b" if self.holding.isupper() else "w"
+
+        self.straightLine(color, x, y)
+        self.diagonalLine(color, x, y)
 
     def background(self):
         for file in range(8):
@@ -68,10 +230,10 @@ class Board:
                     self.display.blit(self.pieces[currentPiece], (x * 60, y * 60))
 
     def __ClickedSquarePosition(self):
-        return [x//60 for x in pygame.mouse.get_pos()]
+        return [x//60 for x in pygame.mouse.get_pos()][::-1]
 
     def pickup(self):
-        rank, file = self.__ClickedSquarePosition()
+        file, rank = self.__ClickedSquarePosition()
         self.holding = self.BOARD[file][rank]
         if self.holding:
             self.BOARD[file][rank] = None
@@ -80,14 +242,24 @@ class Board:
             self.dropZone = None
 
     def drop(self):
-        rank, file = self.__ClickedSquarePosition()
+        file, rank = self.__ClickedSquarePosition()
         if self.holding:
-            # replace the piece
-            self.BOARD[file][rank] = self.holding
-            # change status
-            self.holding = None
-            # change dropzone
-            self.dropZone = [file, rank]
+            if [file, rank] in self.legal_moves and self.__ClickedSquarePosition() != self.pickupZone:
+                # replace the piece
+                self.BOARD[file][rank] = self.holding
+                # change status
+                self.holding = None
+                # change dropzone and clear legal_move list
+                self.dropZone = [file, rank]
+                # play the sound if u chose valid square
+                pygame.mixer.music.play()
+            else:
+                # choose last position
+                file, rank = self.pickupZone
+                self.BOARD[file][rank] = self.holding
+                self.holding = None
+            
+            self.legal_moves.clear()
 
     def update(self):
         self.board()
--- run.py
+++ run.py
@@ -14,23 +14,23 @@ gameDisplay = pygame.display.set_mode((display_width,display_height))
 # set window name
 pygame.display.set_caption('Chess')
 # set window icon
-Icon = pygame.image.load('Pieces/bQueen.png')
+Icon = pygame.image.load('Assets/Pieces/bQueen.png')
 pygame.display.set_icon(Icon)
 
 
 # Images ----------------
-bBishop = pygame.image.load("Pieces/bBishop.png")
-bKing = pygame.image.load("Pieces/bKing.png")
-bKnight = pygame.image.load("Pieces/bKnight.png")
-bPawn = pygame.image.load("Pieces/bPawn.png")
-bQueen = pygame.image.load("Pieces/bQueen.png")
-bRook = pygame.image.load("Pieces/bRook.png")
-wBishop = pygame.image.load("Pieces/wBishop.png")
-wKing = pygame.image.load("Pieces/wKing.png")
-wKnight = pygame.image.load("Pieces/wKnight.png")
-wPawn = pygame.image.load("Pieces/wPawn.png")
-wQueen = pygame.image.load("Pieces/wQueen.png")
-wRook = pygame.image.load("Pieces/wRook.png")
+bBishop = pygame.image.load("Assets/Pieces/bBishop.png")
+bKing = pygame.image.load("Assets/Pieces/bKing.png")
+bKnight = pygame.image.load("Assets/Pieces/bKnight.png")
+bPawn = pygame.image.load("Assets/Pieces/bPawn.png")
+bQueen = pygame.image.load("Assets/Pieces/bQueen.png")
+bRook = pygame.image.load("Assets/Pieces/bRook.png")
+wBishop = pygame.image.load("Assets/Pieces/wBishop.png")
+wKing = pygame.image.load("Assets/Pieces/wKing.png")
+wKnight = pygame.image.load("Assets/Pieces/wKnight.png")
+wPawn = pygame.image.load("Assets/Pieces/wPawn.png")
+wQueen = pygame.image.load("Assets/Pieces/wQueen.png")
+wRook = pygame.image.load("Assets/Pieces/wRook.png")
 
 ImageLocation = {
     "b" : bBishop,
